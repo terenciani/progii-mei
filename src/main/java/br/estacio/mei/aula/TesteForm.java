89680445307b9a4850e7bf6b79f498446f77f3d4
Marcelo <Marcelo@DESKTOP-INVOKHM>
2018-08-28 22:44:43 -0400

    Correção de erros do projeto

---
+++ src/main/java/br/estacio/mei/aula/TesteForm.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.mei.aula;
+
+import main.java.br.estacio.mei.exemplo.LayoutBase;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class TesteForm {
+    public static void main(String[] args) {
+        LayoutBase form = new LayoutBase();
+        form.setVisible(true);
+    }
+}
