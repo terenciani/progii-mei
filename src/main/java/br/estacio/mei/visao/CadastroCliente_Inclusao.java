90cffe16e1ca51d2206f3961dd1ffa15e877a9ee
aluno <aluno@MSCGRESL02014.estacio.acad>
2018-08-28 20:19:46 -0400

    Ajuste Nomenclatura Telas - Cadastro de Clientes

--- src/main/java/br/estacio/mei/visao/CadastroCliente_Inclusao.java
+++ src/main/java/br/estacio/mei/visao/CadastroCliente_Inclusao.java
@@ -8,10 +8,10 @@
  *
  * @author ericvdias
  */
-public class CadastroDeCliente extends javax.swing.JFrame {
+public class CadastroCliente_Inclusao extends javax.swing.JFrame {
 
     /** Creates new form CadastroDeCliente */
-    public CadastroDeCliente() {
+    public CadastroCliente_Inclusao() {
         initComponents();
     }
 
@@ -272,20 +272,21 @@ public class CadastroDeCliente extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CadastroDeCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastroCliente_Inclusao.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CadastroDeCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastroCliente_Inclusao.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CadastroDeCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastroCliente_Inclusao.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CadastroDeCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastroCliente_Inclusao.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new CadastroDeCliente().setVisible(true);
+                new CadastroCliente_Inclusao().setVisible(true);
             }
         });
     }
--- src/main/java/br/estacio/mei/visao/CadastroCliente_TelaInicial.java
+++ src/main/java/br/estacio/mei/visao/CadastroCliente_TelaInicial.java
@@ -8,12 +8,12 @@
  *
  * @author ericvdias
  */
-public class TelaInicial extends javax.swing.JFrame {
+public class CadastroCliente_TelaInicial extends javax.swing.JFrame {
 
     /**
      * Creates new form TelaInicial
      */
-    public TelaInicial() {
+    public CadastroCliente_TelaInicial() {
         initComponents();
     }
 
@@ -137,20 +137,21 @@ public class TelaInicial extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TelaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastroCliente_TelaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TelaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastroCliente_TelaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TelaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastroCliente_TelaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TelaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastroCliente_TelaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TelaInicial().setVisible(true);
+                new CadastroCliente_TelaInicial().setVisible(true);
             }
         });
     }
