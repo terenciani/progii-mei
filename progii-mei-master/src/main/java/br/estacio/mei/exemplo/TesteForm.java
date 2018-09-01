1003e2b1633ce1881a7eca6e50980d9733d3d983
joaoTads <joaosantos.rodrigues9@gmail.com>
2018-09-01 16:45:59 -0400

    atualização de icones do contas a pagar

---
+++ progii-mei-master/src/main/java/br/estacio/mei/exemplo/TesteForm.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.mei.exemplo;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class TesteForm {
+    public static void main(String[] args) {
+        JFrame form = new LayoutBase();
+        form.setVisible(true);
+    }
+}
---
+++ progii-mei-master/src/main/java/br/estacio/mei/visao/categoria/TesteForm.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.mei.visao.categoria;
+
+import javax.swing.JFrame;
+/**
+ *
+ * @author Marcelo
+ */
+public class TesteForm {
+    
+    public static void main(String[] args) {
+        JFrame form = new LayoutBaseCategoria();
+        form.setVisible(true);
+    }
+}
