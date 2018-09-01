057ec4727a247343adfef9ca34f16dce298a5f4b
Marcelo <Marcelo@DESKTOP-INVOKHM>
2018-09-01 17:27:49 -0400

    Remoção de interfaces superfluas

--- src/main/java/br/estacio/mei/exemplo/Sistema.java
+++ src/main/java/br/estacio/mei/exemplo/Sistema.java
@@ -5,15 +5,13 @@
  */
 package br.estacio.mei.exemplo;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 
 /**
  *
  * @author Marcelo
  */
-public class Sistema extends javax.swing.JFrame implements ActionListener{
+public class Sistema extends javax.swing.JFrame {
 
     /**
      * Creates new form Sistema
@@ -423,11 +421,6 @@ public class Sistema extends javax.swing.JFrame implements ActionListener{
                 sistema.setVisible(true);
             }
         });
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 
