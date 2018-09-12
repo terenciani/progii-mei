2504eb5d036e4de3da2b8ab529ec053aff216bf2
aluno <aluno@MSCGRESL02014.estacio.acad>
2018-09-11 20:45:35 -0400

    Criação da estrutura Dao clientes.

---
+++ src/main/java/br/estacio/mei/model/Cliente.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.mei.model;
+
+/**
+ *
+ * @author aluno
+ */
+public class Cliente {
+    private int codigo;
+    private String cpfCnpj;
+    private String nome;
+    private String nomeFantasia;
+    private String inscrEstadual;
+    private int telefone;
+    private String email;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getInscrEstadual() {
+        return inscrEstadual;
+    }
+
+    public void setInscrEstadual(String inscrEstadual) {
+        this.inscrEstadual = inscrEstadual;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+    
+    
+}
