113267ed032113b17977bbaf9f6748618f5d1a10
SaMuB <SaMuB@DESKTOP-TL647JA>
2018-09-13 15:28:31 -0400

    Grupo 3, Daos Relatorio de Estoque

---
+++ src/main/java/br/estacio/mei/model/RelatorioEstoque.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.mei.model;
+
+/**
+ *
+ * @author SaMuB
+ */
+public class RelatorioEstoque {
+    private String pesquisa;
+    
+    public String getPesquisa(){
+        return pesquisa;
+    }
+    
+    
+}
