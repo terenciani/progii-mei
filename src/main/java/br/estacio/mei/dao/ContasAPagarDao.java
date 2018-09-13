99826fde4dd7b1855534cef5c4fd73170f210d23
aluno <aluno@MSCGRESL02020.estacio.acad>
2018-09-13 12:40:08 -0400

    Estruturação de projeto Contas a pagar

---
+++ src/main/java/br/estacio/mei/dao/ContasAPagarDao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.mei.dao;
+import br.estacio.mei.model.ContasAPagar;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author aluno
+ */
+public interface ContasAPagarDao {
+    public ArrayList<ContasAPagar> buscarContasApagarDao();
+
+    public ContasAPagar salvarContasApagarDao(ContasAPagarDao ContasApagarDao);
+
+    public ContasAPagar atualizarContasApagarDao(ContasAPagarDao ContasApagarDao);
+
+    public boolean excluirContasApagarDao(ContasAPagarDao ContasApagarDao);
+}
