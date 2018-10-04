daf1e6897da21c1334ff7f7b5d9b4081dd24e280
Dayves R Dias <dayves.dias@gmail.com>
2018-10-04 11:03:49 -0400

    Mostrando msg de erro ao fazer pesquisa invalida

--- src/main/java/br/estacio/mei/visao/relatorio/estoque/RelatorioDeEstoquePrincipal.java
+++ src/main/java/br/estacio/mei/visao/relatorio/estoque/RelatorioDeEstoquePrincipal.java
@@ -12,6 +12,7 @@ import br.estacio.mei.dao.implementacao.ProdutoDaoEstatico;
 import br.estacio.mei.model.Fornecedor;
 import br.estacio.mei.model.Produto;
 import java.util.ArrayList;
+import javax.swing.JLabel;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -25,6 +26,7 @@ public class RelatorioDeEstoquePrincipal extends javax.swing.JPanel {
      */
     public RelatorioDeEstoquePrincipal() {
         initComponents();
+        lblError.setVisible(false);
         
         Fornecedor fornecedor = new Fornecedor();
         fornecedor.setRazaoSocial("Estacio");
@@ -63,18 +65,33 @@ public class RelatorioDeEstoquePrincipal extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel2 = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
         jPanel10 = new javax.swing.JPanel();
         jScrollPane3 = new javax.swing.JScrollPane();
         txtPesquisar = new javax.swing.JTextPane();
         btnPesquisar = new javax.swing.JButton();
-        jButtonNovoProduto = new javax.swing.JButton();
+        btnTodos = new javax.swing.JButton();
         tipoPesquisa = new javax.swing.JComboBox<>();
         jScrollPane1 = new javax.swing.JScrollPane();
         tbProdutos = new javax.swing.JTable();
-        lblResultado = new javax.swing.JLabel();
+        pnlError = new javax.swing.JPanel();
+        lblError = new javax.swing.JLabel();
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
 
         setLayout(new java.awt.BorderLayout());
+
+        jPanel1.setPreferredSize(new java.awt.Dimension(675, 625));
 
         txtPesquisar.setToolTipText("");
         txtPesquisar.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
@@ -86,10 +103,10 @@ public class RelatorioDeEstoquePrincipal extends javax.swing.JPanel {
             }
         });
 
-        jButtonNovoProduto.setText("Novo Produto");
-        jButtonNovoProduto.addActionListener(new java.awt.event.ActionListener() {
+        btnTodos.setText("Novo Produto");
+        btnTodos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButtonNovoProdutoActionPerformed(evt);
+                btnTodosActionPerformed(evt);
             }
         });
 
@@ -111,17 +128,17 @@ public class RelatorioDeEstoquePrincipal extends javax.swing.JPanel {
                 .addGap(18, 18, 18)
                 .addComponent(btnPesquisar)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
-                .addComponent(jButtonNovoProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(btnTodos, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(19, 19, 19))
         );
         jPanel10Layout.setVerticalGroup(
             jPanel10Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel10Layout.createSequentialGroup()
-                .addContainerGap(39, Short.MAX_VALUE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel10Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(tipoPesquisa, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(jPanel10Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                        .addComponent(jButtonNovoProduto, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnTodos, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGroup(jPanel10Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(btnPesquisar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jScrollPane3))))
@@ -138,73 +155,89 @@ public class RelatorioDeEstoquePrincipal extends javax.swing.JPanel {
         ));
         jScrollPane1.setViewportView(tbProdutos);
 
-        lblResultado.setText("jLabel1");
+        lblError.setText("Errors");
+
+        javax.swing.GroupLayout pnlErrorLayout = new javax.swing.GroupLayout(pnlError);
+        pnlError.setLayout(pnlErrorLayout);
+        pnlErrorLayout.setHorizontalGroup(
+            pnlErrorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlErrorLayout.createSequentialGroup()
+                .addGap(325, 325, 325)
+                .addComponent(lblError)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        pnlErrorLayout.setVerticalGroup(
+            pnlErrorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlErrorLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblError)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(366, 366, 366)
-                .addComponent(lblResultado)
-                .addContainerGap(368, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(jPanel1Layout.createSequentialGroup()
-                    .addContainerGap()
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(jPanel10, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addComponent(jScrollPane1)
-                            .addGap(20, 20, 20)))
-                    .addContainerGap()))
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 736, Short.MAX_VALUE)
+                        .addGap(20, 20, 20))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jPanel10, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(pnlError, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(lblResultado)
-                .addGap(0, 709, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(jPanel1Layout.createSequentialGroup()
-                    .addContainerGap()
-                    .addComponent(jPanel10, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap()
+                .addComponent(jPanel10, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(pnlError, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(28, 28, 28)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(17, Short.MAX_VALUE))
         );
 
         add(jPanel1, java.awt.BorderLayout.CENTER);
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButtonNovoProdutoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonNovoProdutoActionPerformed
-        int linha = tbProdutos.getSelectedRow();
-        int codigo = (int)tbProdutos.getModel().getValueAt(linha, 0);
-        lblResultado.setText(""+codigo);
-    }//GEN-LAST:event_jButtonNovoProdutoActionPerformed
 
     private void btnPesquisarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPesquisarActionPerformed
         int itemSelecionado = tipoPesquisa.getSelectedIndex();
         DefaultTableModel modeloDeColunasDaTabela = (DefaultTableModel)tbProdutos.getModel();
         switch(itemSelecionado) {
             case 0:
+                try {
+                    lblError.setVisible(false);
                     int pesqCodigo = Integer.parseInt(txtPesquisar.getText());
                     while (modeloDeColunasDaTabela.getRowCount() != 0) {
                         modeloDeColunasDaTabela.removeRow(0);
                     }
                     ArrayList<Produto> produtosPorCodigo = produtoDao.pesquisarPorCodigo(pesqCodigo);
                     for (int i=0; i< produtosPorCodigo.size();i++)
-                    {
-                        Produto p = produtosPorCodigo.get(i);
-                        Object[] dadosDaLinha = new Object[6];
-                        dadosDaLinha[0] = p.getCodigo();
-                        dadosDaLinha[1] = p.getNome();
-                        dadosDaLinha[2] = p.getFornecedor().getRazaoSocial();
-                        dadosDaLinha[3] = p.getPrecoCompra();
-                        dadosDaLinha[4] = p.getPrecoVenda();
-                        dadosDaLinha[5] = p.getQuantidade();
-                        modeloDeColunasDaTabela.addRow(dadosDaLinha);
-                    }
-                    break;
+                       {
+                           Produto p = produtosPorCodigo.get(i);
+                           Object[] dadosDaLinha = new Object[6];
+                           dadosDaLinha[0] = p.getCodigo();
+                           dadosDaLinha[1] = p.getNome();
+                           dadosDaLinha[2] = p.getFornecedor().getRazaoSocial();
+                           dadosDaLinha[3] = p.getPrecoCompra();
+                           dadosDaLinha[4] = p.getPrecoVenda();
+                           dadosDaLinha[5] = p.getQuantidade();
+                           modeloDeColunasDaTabela.addRow(dadosDaLinha);
+                       }
+                } catch (Exception e) {
+                    lblError.setText("Pesquisa Invalida!");
+                    lblError.setVisible(true);
+                }
+
+                break;
             case 1:
+                try {
+                    lblError.setVisible(false);
                     String pesqNome = txtPesquisar.getText();
                     while (modeloDeColunasDaTabela.getRowCount() != 0) {
                         modeloDeColunasDaTabela.removeRow(0);
@@ -222,9 +255,15 @@ public class RelatorioDeEstoquePrincipal extends javax.swing.JPanel {
                         dadosDaLinha[4] = p.getPrecoVenda();
                         dadosDaLinha[5] = p.getQuantidade();
                         modeloDeColunasDaTabela.addRow(dadosDaLinha);
-                    }
-                    break;
+                    }    
+                } catch (Exception e) {
+                    lblError.setText("Pesquisa Invalida!");
+                    lblError.setVisible(true);
+                }
+                break;
             case 2:
+                try {
+                    lblError.setVisible(false);
                     String pesqFornecedor = txtPesquisar.getText();
                     while (modeloDeColunasDaTabela.getRowCount() != 0) {
                         modeloDeColunasDaTabela.removeRow(0);
@@ -242,8 +281,14 @@ public class RelatorioDeEstoquePrincipal extends javax.swing.JPanel {
                         dadosDaLinha[5] = p.getQuantidade();
                         modeloDeColunasDaTabela.addRow(dadosDaLinha);
                     }
+                } catch (Exception e) {
+                    lblError.setText("Pesquisa Invalida!");
+                    lblError.setVisible(true);
+                }
                     break;
             case 3:
+                try {
+                    lblError.setVisible(false);
                     int pesqPrecoCompra = Integer.parseInt(txtPesquisar.getText());
                     while (modeloDeColunasDaTabela.getRowCount() != 0) {
                         modeloDeColunasDaTabela.removeRow(0);
@@ -261,8 +306,14 @@ public class RelatorioDeEstoquePrincipal extends javax.swing.JPanel {
                         dadosDaLinha[5] = p.getQuantidade();
                         modeloDeColunasDaTabela.addRow(dadosDaLinha);
                     }
+                } catch (Exception e) {
+                    lblError.setText("Pesquisa Invalida!");
+                    lblError.setVisible(true);
+                }
                     break;
             case 4:
+                try {
+                    lblError.setVisible(false);
                     int pesqPrecoVenda = Integer.parseInt(txtPesquisar.getText());
                     while (modeloDeColunasDaTabela.getRowCount() != 0) {
                         modeloDeColunasDaTabela.removeRow(0);
@@ -280,8 +331,14 @@ public class RelatorioDeEstoquePrincipal extends javax.swing.JPanel {
                         dadosDaLinha[5] = p.getQuantidade();
                         modeloDeColunasDaTabela.addRow(dadosDaLinha);
                     }
+                } catch (Exception e) {
+                    lblError.setText("Pesquisa Invalida!");
+                    lblError.setVisible(true);
+                }
                     break;
             case 5:
+                try {
+                    lblError.setVisible(false);
                     int pesqQtdEstoque = Integer.parseInt(txtPesquisar.getText());
                     while (modeloDeColunasDaTabela.getRowCount() != 0) {
                         modeloDeColunasDaTabela.removeRow(0);
@@ -299,6 +356,10 @@ public class RelatorioDeEstoquePrincipal extends javax.swing.JPanel {
                         dadosDaLinha[5] = p.getQuantidade();
                         modeloDeColunasDaTabela.addRow(dadosDaLinha);
                     }
+                } catch (Exception e) {
+                    lblError.setText("Pesquisa Invalida!");
+                    lblError.setVisible(true);
+                }
                     break;
         }
     }//GEN-LAST:event_btnPesquisarActionPerformed
@@ -307,15 +368,22 @@ public class RelatorioDeEstoquePrincipal extends javax.swing.JPanel {
         // TODO add your handling code here:
     }//GEN-LAST:event_tipoPesquisaActionPerformed
 
+    private void btnTodosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTodosActionPerformed
+        int linha = tbProdutos.getSelectedRow();
+        int codigo = (int)tbProdutos.getModel().getValueAt(linha, 0);
+    }//GEN-LAST:event_btnTodosActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnPesquisar;
-    private javax.swing.JButton jButtonNovoProduto;
+    private javax.swing.JButton btnTodos;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel10;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane3;
-    private javax.swing.JLabel lblResultado;
+    private javax.swing.JLabel lblError;
+    private javax.swing.JPanel pnlError;
     private javax.swing.JTable tbProdutos;
     private javax.swing.JComboBox<String> tipoPesquisa;
     private javax.swing.JTextPane txtPesquisar;
