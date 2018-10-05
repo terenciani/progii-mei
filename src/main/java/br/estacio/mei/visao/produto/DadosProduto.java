50a475e052a8ec549867a393e0b8882f07256a14
aluno <aluno@MSCGRESL02007.estacio.acad>
2018-10-04 22:07:18 -0400

    Atulização dos Jpanel, faltando ajustes no botão de busca de produto

--- src/main/java/br/estacio/mei/visao/produto/DadosProduto.java
+++ src/main/java/br/estacio/mei/visao/produto/DadosProduto.java
@@ -9,12 +9,12 @@ package br.estacio.mei.visao.produto;
  *
  * @author aluno
  */
-public class InserirProduto extends javax.swing.JFrame {
+public class DadosProduto extends javax.swing.JPanel {
 
     /**
-     * Creates new form InserirProduto
+     * Creates new form DadosProduto
      */
-    public InserirProduto() {
+    public DadosProduto() {
         initComponents();
     }
 
@@ -44,10 +44,9 @@ public class InserirProduto extends javax.swing.JFrame {
         jLabel7 = new javax.swing.JLabel();
         jTextField7 = new javax.swing.JTextField();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Inserir Novo Produto");
+        setLayout(new java.awt.BorderLayout());
 
-        Inserir.setBackground(new java.awt.Color(12, 180, 206));
+        Inserir.setBackground(new java.awt.Color(255, 255, 255));
         Inserir.setLayout(null);
 
         jTextField1.addActionListener(new java.awt.event.ActionListener() {
@@ -56,7 +55,7 @@ public class InserirProduto extends javax.swing.JFrame {
             }
         });
         Inserir.add(jTextField1);
-        jTextField1.setBounds(70, 40, 70, 22);
+        jTextField1.setBounds(70, 40, 70, 20);
 
         jLabel1.setText("Código");
         Inserir.add(jLabel1);
@@ -70,7 +69,7 @@ public class InserirProduto extends javax.swing.JFrame {
 
         jLabel3.setText("Quantidade");
         Inserir.add(jLabel3);
-        jLabel3.setBounds(430, 40, 65, 20);
+        jLabel3.setBounds(430, 40, 56, 20);
         Inserir.add(jTextField3);
         jTextField3.setBounds(490, 40, 50, 20);
 
@@ -82,46 +81,33 @@ public class InserirProduto extends javax.swing.JFrame {
             }
         });
         Inserir.add(jButton1);
-        jButton1.setBounds(250, 210, 117, 25);
+        jButton1.setBounds(250, 210, 105, 23);
 
         jLabel4.setText("Valor Atual");
         Inserir.add(jLabel4);
-        jLabel4.setBounds(30, 90, 63, 20);
+        jLabel4.setBounds(30, 90, 52, 20);
         Inserir.add(jTextField4);
-        jTextField4.setBounds(90, 90, 50, 22);
+        jTextField4.setBounds(90, 90, 50, 20);
 
         jLabel5.setText("Valor Antigo");
         Inserir.add(jLabel5);
-        jLabel5.setBounds(170, 90, 70, 20);
+        jLabel5.setBounds(170, 90, 58, 20);
         Inserir.add(jTextField5);
-        jTextField5.setBounds(230, 90, 50, 22);
+        jTextField5.setBounds(230, 90, 50, 20);
 
         jLabel6.setText("Lucro %");
         Inserir.add(jLabel6);
-        jLabel6.setBounds(300, 90, 47, 20);
+        jLabel6.setBounds(300, 90, 40, 20);
         Inserir.add(jTextField6);
-        jTextField6.setBounds(350, 90, 60, 22);
+        jTextField6.setBounds(350, 90, 60, 20);
 
         jLabel7.setText("Descrição");
         Inserir.add(jLabel7);
-        jLabel7.setBounds(30, 130, 55, 20);
+        jLabel7.setBounds(30, 130, 46, 20);
         Inserir.add(jTextField7);
         jTextField7.setBounds(80, 130, 470, 70);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(Inserir, javax.swing.GroupLayout.PREFERRED_SIZE, 585, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(Inserir, javax.swing.GroupLayout.DEFAULT_SIZE, 275, Short.MAX_VALUE)
-        );
-
-        pack();
+        add(Inserir, java.awt.BorderLayout.CENTER);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
@@ -132,40 +118,6 @@ public class InserirProduto extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(InserirProduto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(InserirProduto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(InserirProduto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(InserirProduto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new InserirProduto().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Inserir;
--- src/main/java/br/estacio/mei/visao/produto/ProdutoPrincipal.java
+++ src/main/java/br/estacio/mei/visao/produto/ProdutoPrincipal.java
@@ -27,114 +27,177 @@ public class ProdutoPrincipal extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        Inserir = new javax.swing.JPanel();
-        jTextField1 = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
-        jLabel3 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
+        panelDinamico = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
-        jLabel4 = new javax.swing.JLabel();
-        jTextField4 = new javax.swing.JTextField();
-        jLabel5 = new javax.swing.JLabel();
-        jTextField5 = new javax.swing.JTextField();
-        jLabel7 = new javax.swing.JLabel();
-        jTextField6 = new javax.swing.JTextField();
-        jLabel8 = new javax.swing.JLabel();
-        jTextField7 = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
 
         setLayout(new java.awt.BorderLayout());
 
-        Inserir.setBackground(new java.awt.Color(12, 180, 206));
-        Inserir.setLayout(null);
+        panelDinamico.setLayout(new java.awt.BorderLayout());
 
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
-            }
-        });
-        Inserir.add(jTextField1);
-        jTextField1.setBounds(240, 40, 70, 20);
-
-        jLabel1.setText("Código");
-        Inserir.add(jLabel1);
-        jLabel1.setBounds(200, 40, 40, 20);
-
-        jLabel2.setText("Nome");
-        Inserir.add(jLabel2);
-        jLabel2.setBounds(410, 40, 30, 20);
-        Inserir.add(jTextField2);
-        jTextField2.setBounds(440, 40, 200, 20);
-
-        jLabel3.setText("Quantidade");
-        Inserir.add(jLabel3);
-        jLabel3.setBounds(650, 40, 56, 20);
-        Inserir.add(jTextField3);
-        jTextField3.setBounds(710, 40, 50, 20);
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setToolTipText("");
 
         jButton1.setBackground(new java.awt.Color(102, 102, 102));
-        jButton1.setText("Inserir Produto");
+        jButton1.setText("Novo Produto");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
-        Inserir.add(jButton1);
-        jButton1.setBounds(450, 390, 105, 23);
 
-        jLabel4.setText("Valor Atual");
-        Inserir.add(jLabel4);
-        jLabel4.setBounds(200, 120, 52, 20);
-        Inserir.add(jTextField4);
-        jTextField4.setBounds(260, 120, 50, 20);
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null}
+            },
+            new String [] {
+                "Código ", "Nome", "Opções", "Quantidade", "Valor Atual", "Valor Antigo", "Lucro %", "Descrição"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
 
-        jLabel5.setText("Valor Antigo");
-        Inserir.add(jLabel5);
-        jLabel5.setBounds(400, 120, 58, 20);
-        Inserir.add(jTextField5);
-        jTextField5.setBounds(470, 120, 50, 20);
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 784, Short.MAX_VALUE)
+                .addContainerGap())
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(291, 291, 291)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(67, 67, 67)
+                .addComponent(jButton1)
+                .addGap(37, 37, 37)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 322, Short.MAX_VALUE))
+        );
 
-        jLabel7.setText("Lucro %");
-        Inserir.add(jLabel7);
-        jLabel7.setBounds(650, 120, 40, 20);
-        Inserir.add(jTextField6);
-        jTextField6.setBounds(700, 120, 60, 20);
+        panelDinamico.add(jPanel2, java.awt.BorderLayout.CENTER);
 
-        jLabel8.setText("Descrição");
-        Inserir.add(jLabel8);
-        jLabel8.setBounds(210, 260, 46, 20);
-        Inserir.add(jTextField7);
-        jTextField7.setBounds(270, 260, 470, 70);
-
-        add(Inserir, java.awt.BorderLayout.CENTER);
+        add(panelDinamico, java.awt.BorderLayout.CENTER);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
-
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+       DadosProduto dadosProduto = new DadosProduto();
+        panelDinamico.removeAll();
+        panelDinamico.add(dadosProduto);
+        panelDinamico.validate();
+        panelDinamico.repaint();
     }//GEN-LAST:event_jButton1ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel Inserir;
     private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
-    private javax.swing.JTextField jTextField6;
-    private javax.swing.JTextField jTextField7;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JPanel panelDinamico;
     // End of variables declaration//GEN-END:variables
 }
