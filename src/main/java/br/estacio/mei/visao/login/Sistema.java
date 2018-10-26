7f3368ebf32508daa9a450cbb424e6b91d9888c4
aluno <aluno@MSCGRESL02001.estacio.acad>
2018-10-25 22:03:57 -0300

    Habilitado Fechar, Maximizar e Minimizar na tela Sistema.

--- src/main/java/br/estacio/mei/visao/login/Sistema.java
+++ src/main/java/br/estacio/mei/visao/login/Sistema.java
@@ -125,8 +125,8 @@ public class Sistema extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setUndecorated(true);
-        setResizable(false);
+        setTitle("Sistema de Apoio ao Microempreendedor");
+        setAlwaysOnTop(true);
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowOpened(java.awt.event.WindowEvent evt) {
                 formWindowOpened(evt);
